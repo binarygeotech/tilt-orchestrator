dacad949cdb46b0c0026540cd6136544f0930cb8
George Okojie Davis <binarygeotech@gmail.com>
2026-01-04 03:59:33 +0000

    refactor: improve code formatting and organization in various modules

--- src-tauri/src/backend/mod.rs
+++ src-tauri/src/backend/mod.rs
@@ -1,9 +1,9 @@
 pub mod generator;
-pub mod tilt_manager;
 pub mod git;
 pub mod project;
+pub mod tilt_manager;
 // pub mod project_manager;
 pub mod dependency_graph;
+pub mod errors;
 pub mod ipc;
 pub mod ports;
-pub mod errors;
