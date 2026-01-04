45a489fccbe7c8243d7219e4c2ba69f84bcce432
George Okojie Davis <binarygeotech@gmail.com>
2026-01-04 04:49:48 +0000

    refactor: simplify check_tilt_process_running to always return true for valid PID

--- src-tauri/src/backend/tilt_manager.rs
+++ src-tauri/src/backend/tilt_manager.rs
@@ -84,8 +84,7 @@ fn check_tilt_process_running(pid: Option<u32>) -> bool {
         {
             println!("{}", pid_val);
             // On Windows, just assume it's running if we have a PID
-            
-            pid_val > 0;
+            true
         }
     } else {
         false
