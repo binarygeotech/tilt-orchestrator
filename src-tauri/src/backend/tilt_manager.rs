668b2488ea7ca7f056a8951d099433f129953127
George Okojie Davis <binarygeotech@gmail.com>
2026-01-04 04:47:11 +0000

    refactor: fix check_tilt_process_running to return boolean correctly

--- src-tauri/src/backend/tilt_manager.rs
+++ src-tauri/src/backend/tilt_manager.rs
@@ -85,7 +85,7 @@ fn check_tilt_process_running(pid: Option<u32>) -> bool {
             println!("{}", pid_val);
             // On Windows, just assume it's running if we have a PID
             
-            pid_val > 0
+            pid_val > 0;
         }
     } else {
         false
